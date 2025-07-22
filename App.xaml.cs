a72179e63c47d809be8f1e103e62562275e2f425
viniciusyoshino <vinicius.yoshino@icloud.com>
2025-07-22 13:29:25 -0300

    Commit inicial

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Tabela;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tabela;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
